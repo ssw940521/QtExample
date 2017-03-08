8c18aa06d5bc6fb7438c7152a4a124a57918aab6
ssw940521 <450705838@qq.com>
2017-03-08 16:23:22 +0800

    123

---
+++ DialogExample/inputdlg.cpp
@@ -0,0 +1,6 @@
+#include "inputdlg.h"
+
+InputDlg::InputDlg()
+{
+
+}
---
+++ DialogExample/inputdlg.h
@@ -0,0 +1,11 @@
+#ifndef INPUTDLG_H
+#define INPUTDLG_H
+
+
+class InputDlg : public QDialog
+{
+public:
+    InputDlg();
+};
+
+#endif // INPUTDLG_H
